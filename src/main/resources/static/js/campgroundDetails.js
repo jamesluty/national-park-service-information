f5b31fa7eaffb25c57f99d3f9f127d67e6841fb8
Selena Nguyen <missselenah@gmail.com>
2022-07-15 15:03:13 -0700

    campgroundDetails bugs

--- src/main/resources/static/js/campgroundDetails.js
+++ src/main/resources/static/js/campgroundDetails.js
@@ -19,8 +19,12 @@ async function renderCampgrounds(){
 	for (let campground of campgrounds) {
 		if(campground.id === id){
 			let fees = "";
+			let phoneNumber = "";
+			let emailAddress = "";
 			let hours = "";
 			let addresses = "";
+			let url = "";
+			let directionsUrl = "";
 			let images = campground.images;
 			let image = "";	
 	
@@ -33,28 +37,53 @@ async function renderCampgrounds(){
 			
 			campground.images.forEach(image => {
 				photo.push(image.url);
-			})	
-			
-			campground.fees.forEach(fee => {
-				let feeHtml = 
-					`<p><b>${fee.title}</b> - $${fee.cost} <br>
-					${fee.description}<br></p>`;
-				fees += feeHtml;
-			})	
-	
-			campground.operatingHours.forEach(item => {
-				let itemHtml =			
-				`Sunday - ${item.standardHours.sunday}<br>
-				Monday - ${item.standardHours.monday}<br>
-				Tuesday - ${item.standardHours.tuesday}<br>
-				Wednesday - ${item.standardHours.wednesday}<br>
-				Thursday - ${item.standardHours.thursday}<br>
-				Friday - ${item.standardHours.friday}<br>
-				Saturday - ${item.standardHours.saturday}<br>
-				<br>
-				<i>*** ${item.description}</i>`;
-				hours += itemHtml;
 			})
+			
+			
+			if(campground.contacts.phoneNumbers.length>=1){
+				phoneNumber = campground.contacts.phoneNumbers[0].phoneNumber;
+			}else{
+				phoneNumber = "<i>Not Available</i>";
+			}
+			
+			
+			if(campground.contacts.emailAddresses.length>=1){
+				emailAddress = campground.contacts.emailAddresses[0].emailAddress;
+			}else{
+				emailAddress = "<i>Not Available</i>";
+			}
+			
+			
+			if (campground.fees.length>=1){
+				campground.fees.forEach(fee => {
+					let feeHtml = 
+						`<p><b>${fee.title}</b> - $${fee.cost} <br>
+						${fee.description}<br></p>`;
+					fees += feeHtml;
+				})				
+			}else{
+				fees = "<i>Not Available</i>";
+			}
+	
+	
+			if (campground.operatingHours.length>=1){
+				campground.operatingHours.forEach(item => {
+					let itemHtml =			
+					`Sunday - ${item.standardHours.sunday}<br>
+					Monday - ${item.standardHours.monday}<br>
+					Tuesday - ${item.standardHours.tuesday}<br>
+					Wednesday - ${item.standardHours.wednesday}<br>
+					Thursday - ${item.standardHours.thursday}<br>
+					Friday - ${item.standardHours.friday}<br>
+					Saturday - ${item.standardHours.saturday}<br>
+					<br>
+					<i>*** ${item.description}</i>`;
+					hours += itemHtml;
+				})				
+			}else{
+				hours = "<i>Not Available</i>";
+			}
+
 			
 			let campsites = 
 				`Total Sites: ${campground.campsites.totalSites}</br>
@@ -65,11 +94,20 @@ async function renderCampgrounds(){
 				Boat Access: ${campground.campsites.walkBoatTo}</br>`
 			
 			campground.addresses.forEach(address => {
-				let adrHtml =
-				`<h5>${address.type}</h5>
-				<p>${address.line1} ${address.line2}, ${address.city}, ${address.stateCode}, ${address.postalCode}</p>`;
-				addresses += adrHtml;
+				if (address.line1.length>=1){
+					let adrHtml =
+					`<h5>${address.type}</h5>
+					<p>${address.line1} ${address.line2}, ${address.city}, ${address.stateCode}, ${address.postalCode}</p>`;
+					addresses += adrHtml;
+				} else {
+					let adrHtml =
+					`<h5>${address.type}</h5>
+					<p>${address.city}, ${address.stateCode}, ${address.postalCode}</p>`;
+					addresses += adrHtml;
+				}
+				
 			})
+			
 			
 			let amenities = 
 				`Toilets - <i>${campground.amenities.toilets}</i><br>
@@ -77,7 +115,14 @@ async function renderCampgrounds(){
 				Showers - <i>${campground.amenities.showers}</i><br>
 				Potable Water - <i>${campground.amenities.potableWater}</i><br>
 				Firewood for Sale - <i>${campground.amenities.firewoodForSale}</i><br>`
-						
+			
+			if (campground.url.length>=1){
+				url = `<a class="btn btn-outline-success" href="${campground.url}">Website</a><br>`
+			}	
+			
+			if (campground.directionsUrl.length>=1){
+				directionsUrl = `<a class="btn btn-outline-warning mt-2" href="${campground.directionsUrl}">Directions</a><br>`
+			}	
 			
 			let htmlSegment = 
 				`<div class="campgroundDetails">
@@ -91,7 +136,7 @@ async function renderCampgrounds(){
 					<div class="imgDiv">
 						<button class="btn btn-secondary" onclick="imgLeft()"><</button>
 						<div id="rotateImg">					
-							<img id="detailsImg" src="${campground.images[0].url}"/>
+							<img id="detailsImg" src="${image}"/>
 						</div>
 						<button class="btn btn-secondary" onclick="imgRight()">></button>
 					</div>
@@ -143,12 +188,12 @@ async function renderCampgrounds(){
 							<h5>Information:</h5>
 							<div class="pDiv">
 								<div>
-									<a class="btn btn-outline-success" href="${campground.url}">Website</a><br>
-									<a class="btn btn-outline-warning mt-2" href="${campground.directionsUrl}">Directions</a><br>
+									${url}
+									${directionsUrl}
 								</div>
 								<br>
-								<p>Phone Number: ${campground.contacts.phoneNumbers[0].phoneNumber}<br>
-								Email Address: ${campground.contacts.emailAddresses[0].emailAddress}</p>
+								<p>Phone Number: ${phoneNumber}<br>
+								Email Address: ${emailAddress}</p>
 							</div>
 						</div>						
 					</div>
