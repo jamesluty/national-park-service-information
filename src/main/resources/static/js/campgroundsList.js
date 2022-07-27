ccba85f6ef5c8aa2eb10716021aa482f3728856f
Selena Nguyen <missselenah@gmail.com>
2022-07-27 14:30:28 -0700

    no campgrounds available

--- src/main/resources/static/js/campgroundsList.js
+++ src/main/resources/static/js/campgroundsList.js
@@ -13,18 +13,25 @@ async function getCampgrounds(){
 async function renderCampgrounds(){
 	let campgrounds = await getCampgrounds();
 	let html = '';
-	campgrounds.forEach(campground => {
+	if(campgrounds.length===0){
+		let htmlSegment = 
+			`<div class="text-center">
+				<p class="fs-4 fst-italic text-color">*** No Campgrounds Available ***</p>
+			</div>`
+		html += htmlSegment;
+	}
+	for(let campground of campgrounds){
 		if(campground.addresses.length>0){
 			if(campground.addresses[0].stateCode===state){
-				
+
 				let images = campground.images;
 				let image = "";
 				let city = "";
 				let state = "";
+				let reservationInfo = "";
 				
 				if (images.length>=1){
 					image = images[0].url;
-					console.log(image)
 				}else{
 					image = "https://vipvoice.files.wordpress.com/2021/06/arrowhead.jpg";
 				}
@@ -32,6 +39,10 @@ async function renderCampgrounds(){
 				if (campground.addresses.length>0){
 					city = campground.addresses[0].city + ",";
 					state = campground.addresses[0].stateCode;
+				}
+				
+				if (campground.reservationInfo.length>0){
+					reservationInfo = campground.reservationInfo;
 				}
 				
 				let htmlSegment = 
@@ -49,16 +60,30 @@ async function renderCampgrounds(){
 									<a class="btn btn-dark" href="/campgrounds/details/${campground.id}">View details</a>
 								</div>
 							</div>
-							<p>${campground.reservationInfo}</p>
+							<p>${reservationInfo}</p>
 							<p>${campground.description}</p>
 								
 						</div>
 					</div>`;
 				html += htmlSegment;
-			}
+			}else{
+				let htmlSegment = 
+					`<div class="text-center">
+						<p class="fs-4 fst-italic text-color">*** No Campgrounds Available ***</p>
+					</div>`
+				html += htmlSegment;
+				break;
+			} 
+		}else{
+				let htmlSegment = 
+					`<div class="text-center">
+						<p class="fs-4 fst-italic text-color">*** No Campgrounds Available ***</p>
+					</div>`
+				html += htmlSegment;
+				break;
 		}
 
-	})
+	}
 	
 	let container = document.querySelector(".container");
 	container.innerHTML = html;
