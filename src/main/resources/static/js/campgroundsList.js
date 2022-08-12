a58501b121c113c838719497e2e8e35da75740db
Selena Nguyen <missselenah@gmail.com>
2022-08-12 14:33:24 -0700

    reverted state address constraints

--- src/main/resources/static/js/campgroundsList.js
+++ src/main/resources/static/js/campgroundsList.js
@@ -20,68 +20,52 @@ async function renderCampgrounds(){
 			</div>`
 		html += htmlSegment;
 	}
-	for(let campground of campgrounds){
-		if(campground.addresses.length>0){
-			if(campground.addresses[0].stateCode===state){
+	for(let campground of campgrounds){		
+		
 
-				let images = campground.images;
-				let image = "";
-				let city = "";
-				let state = "";
-				let reservationInfo = "";
-				
-				if (images.length>=1){
-					image = images[0].url;
-				}else{
-					image = "https://vipvoice.files.wordpress.com/2021/06/arrowhead.jpg";
-				}
-				
-				if (campground.addresses.length>0){
-					city = campground.addresses[0].city + ",";
-					state = campground.addresses[0].stateCode;
-				}
-				
-				if (campground.reservationInfo.length>0){
-					reservationInfo = campground.reservationInfo;
-				}
-				
-				let htmlSegment = 
-					`<div class="campground">
-						<div class="imgSize">
-							<img class="listImg" src="${image}" alt="No Image Available"/>
-						</div>
-						<div class="listDetails">
-							<div class="listHeader">
-								<div>			
-									<h3 class="">${campground.name}</h3>
-									<p><i>${city} ${state}</i></p>
-								</div>
-								<div class="btnDiv">
-									<a class="btn btn-dark" href="/campgrounds/details/${campground.id}">View details</a>
-								</div>
-							</div>
-							<p>${reservationInfo}</p>
-							<p>${campground.description}</p>
-								
-						</div>
-					</div>`;
-				html += htmlSegment;
-			}else{
-				let htmlSegment = 
-					`<div class="text-center">
-						<p class="fs-4 fst-italic text-color">*** No Campgrounds Available ***</p>
-					</div>`
-				html += htmlSegment;
-				break;
-			} 
+		let images = campground.images;
+		let image = "";
+		let city = "";
+		let state = "";
+		let reservationInfo = "";
+		
+		if (images.length>=1){
+			image = images[0].url;
 		}else{
-				let htmlSegment = 
-					`<div class="text-center">
-						<p class="fs-4 fst-italic text-color">*** No Campgrounds Available ***</p>
-					</div>`
-				html += htmlSegment;
-				break;
+			image = "https://vipvoice.files.wordpress.com/2021/06/arrowhead.jpg";
 		}
+		
+		if (campground.addresses.length>0){
+			city = campground.addresses[0].city + ",";
+			state = campground.addresses[0].stateCode;
+		}
+		
+		if (campground.reservationInfo.length>0){
+			reservationInfo = campground.reservationInfo;
+		}
+		
+		let htmlSegment = 
+			`<div class="campground">
+				<div class="imgSize">
+					<img class="listImg" src="${image}" alt="No Image Available"/>
+				</div>
+				<div class="listDetails">
+					<div class="listHeader">
+						<div>			
+							<h3 class="">${campground.name}</h3>
+							<p><i>${city} ${state}</i></p>
+						</div>
+						<div class="btnDiv">
+							<a class="btn btn-dark" href="/campgrounds/details/${campground.id}">View details</a>
+						</div>
+					</div>
+					<p>${reservationInfo}</p>
+					<p>${campground.description}</p>
+						
+				</div>
+			</div>`;
+		html += htmlSegment;
+		 
 
 	}
 	
