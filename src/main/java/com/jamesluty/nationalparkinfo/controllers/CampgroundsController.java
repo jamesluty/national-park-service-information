2a11748d07efd5cbfe7e718bb7a57645ab8f2e44
Selena Nguyen <missselenah@gmail.com>
2022-08-31 10:29:08 -0700

    South Dakota campground name

--- src/main/java/com/jamesluty/nationalparkinfo/controllers/CampgroundsController.java
+++ src/main/java/com/jamesluty/nationalparkinfo/controllers/CampgroundsController.java
@@ -13,7 +13,7 @@ public class CampgroundsController {
 	@GetMapping("/campgrounds/{state}")
 	public String state(@PathVariable("state") String state, Model model) {
 		String stateAbr = StringUtils.substringBetween(state, "(", ")");
-		String stateFull = StringUtils.substringBefore(state, " ");
+		String stateFull = StringUtils.substringBefore(state, "(");
 		
 		model.addAttribute("stateAbr", stateAbr);
 		model.addAttribute("stateFull", stateFull);
