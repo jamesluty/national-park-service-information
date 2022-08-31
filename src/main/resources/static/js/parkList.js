8f68554b4f7543abfb1dee7e48ba77fd18865f02
James Luty <luty0125@gmail.com>
2022-08-31 10:29:56 -0700

    fixed state name and default image

--- src/main/resources/static/js/parkList.js
+++ src/main/resources/static/js/parkList.js
@@ -13,17 +13,26 @@ async function getParks(){
 async function renderParks(){
 	let parks = await getParks();
 	let html = '';
+	
+	
 	parks.forEach(park => {
+		let image = "";
+		
+		if (park.images.length>=1){
+			image = park.images[0].url;
+		}else{
+			image = "https://vipvoice.files.wordpress.com/2021/06/arrowhead.jpg";
+		}
 		let htmlSegment = 
 			`<div class="park">
 				<div class=listImgDiv>
-					<img class="listImg" src="${park.images[0].url}"/>
+					<img class="listImg" src="${image}"/>
 				</div>
 				<div class="listDetails">
 					<div class="listHeader">
 						<h3 class="listItemTitle">${park.name}</h3>
 						<div class=detailBtnDiv>
-							<a class="btn btn-dark" href="/parks/details/${park.parkCode}">View details</a>
+							<a class="btn btn-dark smBtn" href="/parks/details/${park.parkCode}">View details</a>
 						</div>						
 					</div>
 					<p>${park.description}</p>
